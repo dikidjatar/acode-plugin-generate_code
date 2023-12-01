741a3ca90f4f5c09ab2695c5c48d996e60cee91d
dikidjatar <dikidjatar@gmail.com>
2023-12-01 23:24:11 +0700

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,23 @@
+# Generate Code With AI
+
+**Generate Code** with AI is a powerful code generation plugin for Acode. With the help of AI, this plugin automates the process of writing code snippets, making your coding experience faster and more efficient.
+
+## Features
+- **AI-Powered Code Generation**: Leverage the capabilities of AI to instantly generate code based on your commands.
+- **Seamless Integration**: Effortlessly integrate the plugin into your preferred code editor, enhancing your workflow without any hassle.
+
+## How To Use
+To use **Generate Code**, search for `Generate Code` in the **command palette (•••)** or use the side button on the right side of the editor in the vertical direction. Note: Place the cursor at the exact location where you want to insert the code..
+
+Type `Show Generate Code Button` to display the right button
+
+Type `Hide Generate Code Button` to hide the right button.
+
+To update your token, search for `"Generate Code Update Token"`.
+
+
+## How To Get API Token
+To obtain a token, visit [https://platform.openai.com/api-keys](https://platform.openai.com/api-keys), simply click the "create new secret key" button to generate the token. If you don't have an account yet, please create one first.
+
+# Support
+For any issues, bug reports, or feature requests, please [submit an issue](https://gothub.com/dikidjatar/acode-plugin-generate_token).
---
+++ src/main.js
@@ -0,0 +1,151 @@
+import plugin from '../plugin.json';
+
+import { Configuration, OpenAIApi } from 'openai';
+
+const SideButton = acode.require('sideButton');
+const prompt = acode.require('prompt');
+const multiPrompt = acode.require('multiPrompt');
+
+class GenerateCode {
+  
+  async init() {
+    
+    this.$markdownItFile = tag("script", {
+      src: this.baseUrl + "assets/markdown-it.min.js"
+    });
+    
+    document.head.append(this.$markdownItFile);
+
+    // Command
+    editorManager.editor.commands.addCommand({
+      name: 'generate_code',
+      description: 'Generate Code',
+      exec: this.run.bind(this),
+    });
+
+    editorManager.editor.commands.addCommand({
+      name: 'hide_generate_code_btn',
+      description: 'Hide the generate code button',
+      exec: () => this.sideButton.hide()
+    });
+    
+    editorManager.editor.commands.addCommand({
+      name: 'show_generate_code_btn',
+      description: 'Show the generate code button',
+      exec: () => this.sideButton.show()
+    });
+
+    editorManager.editor.commands.addCommand({
+      name: 'generate_code_update_token',
+      description: 'Update Openai Token (Generate Code)',
+      exec: this.updateToken.bind(this),
+    });
+
+    try {
+      this.sideButton = SideButton({
+        text: 'Generate Code',
+        icon: 'generate-code-icon',
+        onclick: () => this.run(),
+        backgroundColor: '#8400ff',
+        textColor: '#000'
+      });
+
+      this.sideButton.show();
+
+    } catch (error) {
+      window.alert(error);
+    }
+
+  }
+
+  async run() {
+    let token;
+    const openaiToken = window.localStorage.getItem('generate-code-token');
+    if (openaiToken) {
+      token = openaiToken;
+    } else {
+      let tokenPrompt = await multiPrompt(
+        'Enter your openai api key',
+        [{
+          type: 'text',
+          id: 'aicode-token',
+          required: true,
+          placeholder: 'Enter your openai api key'
+        }],
+        'https://platform.openai.com/api-keys'
+      )
+      token = tokenPrompt['aicode-token'];
+      window.localStorage.setItem('generate-code-token', token);
+    }
+
+    const openai = new OpenAIApi(new Configuration({ apiKey: token}))
+    let mdIt = window.markdownit({
+      html: false,
+      xhtmlOut: false,
+      breaks: false,
+      linkify: false,
+      typographer: false,
+      quotes: '“”‘’',
+      highlight: function (str) {editorManager.editor.insert(str)}
+    });
+
+    const options = {placeholder: 'Enter a command to generate code...'}
+
+    const userPrompt = await prompt('Generate Code', '', 'text', options);
+    const res = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        { role: 'system', content: 'You are ChatGPT, a large language model trained by OpenAI.'},
+        { role: 'user', content: userPrompt}
+      ],
+      temperature: 0
+    })
+    
+    let result = res.data.choices[0].message.content;
+    mdIt.render(result);
+  }
+
+  async updateToken() {
+    let newToken = await multiPrompt(
+      'Enter your openai api key',
+      [{
+        type: 'text',
+        id: 'aicode-token',
+        required: true,
+        placeholder: 'Enter your openai api key'
+      }],
+      'https://platform.openai.com/api-keys'
+      )
+
+    window.localStorage.removeItem('generate-code-token');
+    window.localStorage.setItem('generate-code-token', newToken['aicode-token']);
+    window.toast('Api Key Update!', 3000);
+  }
+
+  async destroy() {
+    editorManager.editor.commands.removeCommand('generate_code');
+    editorManager.editor.commands.removeCommand('generate_code_update_token');
+    editorManager.editor.commands.removeCommand('hide_generate_code_btn');
+    editorManager.editor.commands.removeCommand('show_generate_code_btn');
+    window.localStorage.removeItem('generate-code-token');
+    this.$markdownItFile.remove();
+  }
+
+}
+
+if (window.acode) {
+  const acodePlugin = new GenerateCode();
+  acode.setPluginInit(
+    plugin.id,
+    (baseUrl, $page, { cacheFileUrl, cacheFile }) => {
+      if (!baseUrl.endsWith("/")) {
+        baseUrl += "/";
+      }
+      acodePlugin.baseUrl = baseUrl;
+      acodePlugin.init($page, cacheFile, cacheFileUrl);
+    }
+  );
+  acode.setPluginUnmount(plugin.id, () => {
+    acodePlugin.destroy();
+  });
+}
